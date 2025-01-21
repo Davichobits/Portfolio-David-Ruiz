2366a62c002878b8738d88a49b737ea743a98f01
Davichobits <davidruizdeveloper@gmail.com>
2025-01-21 17:27:59 -0500

    BentoGrid created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,42 @@
-import Image from "next/image";
+// components
 import Hero from "@/components/Hero";
+import { Grid } from "@/components/Grid";
+// ui
+import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
+// icons
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+    icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "About",
+    link: "/about",
+    icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+    icon: (
+      <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+    ),
+  },
+];
 
 export default function Home() {
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNavbar
+          navItems={navItems}
+        />
         <Hero />
+        <Grid />
+        <div className="h-80"></div>
       </div>
     </main>
   );
---
+++ components/Grid.tsx
@@ -0,0 +1,5 @@
+export const Grid = () => {
+  return (
+    <div>Grid</div>
+  )
+}
