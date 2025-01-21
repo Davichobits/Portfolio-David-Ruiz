f79c43472aad73caf6cc39c1e1e3c7531065e01d
Davichobits <davidruizdeveloper@gmail.com>
2025-01-21 15:38:47 -0500

    Adding profile img to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,7 @@
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import profileImg from '@/assets/profile_green.webp'
+import Image from 'next/image';
 
 const Hero = () => {
   return (
@@ -20,6 +22,7 @@ const Hero = () => {
       </div>
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+        <Image className='rounded-full w-40 h-40' src={profileImg} alt="Descripción de la imagen"/>
           <h2 className='uppercase tracking-widest text-2xl text-center text-blue-100 max-w-80'>
             David Ruiz
           </h2>
