c445e92703e84ba80d13e1fe42090b66bb1b2ff9
Davichobits <davidruizdeveloper@gmail.com>
2025-01-22 17:46:30 -0500

    Assets added

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,5 +1,21 @@
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid"
+import { gridItems } from "@/data"
+
 export const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id="about">
+      {gridItems.map(({ id, title, description, className, img, imgClassName, titleClassName, spareImg }) => (
+        <BentoGridItem
+          key={id}
+          title={title}
+          description={description}
+          className={className}
+          img={img}
+          imgClassName={imgClassName}
+          titleClassName={titleClassName}
+          spareImg={spareImg}
+        />
+      ))}
+    </section>
   )
 }
